c5d1b43758693ab623cf1dbc2e0dbc517a0e944f
YonchoBoy <m.angelimichelle@gmail.com>
2026-07-11 17:00:43 +0800

    Update 3.7:
    the player have now 3 lives but you can't see how much you died.

--- my-1-st-platformer-game/Scripts/main_character.gd
+++ my-1-st-platformer-game/Scripts/main_character.gd
@@ -1,16 +1,22 @@
 extends CharacterBody2D
 
 #The Constants.
-const SPEED = 400.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 
 #The Variables.
 var Jump_Count = 0
+var Dash_Speed = 600
+var HP = 3
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if HP <= 0:
+		print("YOU DIED!")
+		get_tree().reload_current_scene()
 
 	# This handles our jump and our double jump.
 	if Input.is_action_just_pressed("Jump") and Jump_Count == 1:
@@ -26,7 +32,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
+	var direction := Input.get_axis("Left", "Right")	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
